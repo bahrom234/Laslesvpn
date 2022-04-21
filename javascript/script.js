b47585a5414b248f047f6f14fadfa4c584732d26
Mirafzal <93206764+bahrom234@users.noreply.github.com>
2022-04-21 08:02:41 +0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+// alert("Hello, to our VPN site LaslesVPN");
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    const mainMenu = document.querySelector('.mainMenu');
+    const closeMenu = document.querySelector('.closeMenu');
+    const openMenu = document.querySelector('.openMenu');
+    openMenu.addEventListener('click', show);
+    closeMenu.addEventListener('click', close);
+
+    function show() {
+        mainMenu.style.display = 'flex';
+        mainMenu.style.top = '';
+    }
+
+    function close() {
+        mainMenu.style.top = '-100%';
+    }
+})
